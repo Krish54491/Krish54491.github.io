b26e743cad15aa9c493d8bc356f751faa9555ba4
Krish54491 <krishbharal@gmail.com>
2024-11-21 21:12:08 -0600

    Made tic-tac toe

--- krish544/src/App.jsx
+++ krish544/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { TicTacToe } from "./TicTacToe";
+import { Countdown } from "./Countdown";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let mode = "Tic-Tac-Toe";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  if (mode === "Tic-Tac-Toe") return <TicTacToe />;
+  else if (mode === "Countdown") return <Countdown />;
+  else {
+    return <button></button>;
+  }
 }
 
-export default App
+export default App;
